f63ff5b941c240b4adbe06c0f49c973dc8165930
Alec Allain <allainal@mail.gvsu.edu>
2020-01-22 15:30:54 -0500

    Basic building blocks

---
+++ Java-Calculator/Calculator.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.util.*;
+
+/**
+ * This class simulates a basic calculator.
+ * 
+ * @author Alec Allain 
+ * @version 1/20/20
+ */
+public class Calculator {
+    
+    /**
+     * Constructor
+     */
+    public Calculator() {
+        
+    }
+    
+    public double add(double x, double y) {
+        double result = 0;
+        result = x + y;
+        return result;
+    }
+    
+    public double subtract(double x, double y) {
+        double result = 0;
+        result = x + y;
+        return result;
+    }
+    
+    public double multiply(double x, double y) {
+        double result = 0;
+        result = x + y;
+        return result;
+    }
+    
+    public double divide(double x, double y) {
+        double result = 0;
+        result = x + y;
+        return result;
+    }
+    
+    public static void main (Stirng [] args) {
+        
+    }
+}
---
+++ Java-Calculator/CalculatorGUI.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+/**
+ * This class simulates the graphical user interface for the calculator.
+ * 
+ * @author Alec Allain
+ * @version 1/20/20
+ */
+public class CalculatorGUI extends JFrame implements ActionListener {
+    /** The actual calcualtor object */
+    private Calculator calc;
+    
+    /** Buttons for numbers and functions */
+    private JButton zero;
+    private JButton one;
+    private JButton two;
+    private JButton three;
+    private JButton four;
+    private JButton five;
+    private JButton six;
+    private JButton seven;
+    private JButton eight;
+    private JButton nine;
+    private JButton add;
+    private JButton sub;
+    private JButton mul;
+    private JButton div;
+    private JButton clear;
+    private JButton sqrt;
+    private JButton percent;
+    private JButton period;
+    
+    public CalculatorGUI() {
+        calc = new Calculator();
+    }
+    
+    public void actionPerformed (ActionEvent e) {
+        
+    }
+    
+    public static void main(String [] args) {
+        CalculatorGUI gui = new CalculatorGUI();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setTitle("Java Calculator");
+        gui.pack();
+        gui.setVisible(true);
+    }
+}
