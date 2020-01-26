a5b109d0f3a0387525f4e95ab0674b9c85f29807
Alec Allain <allainal@mail.gvsu.edu>
2020-01-26 18:55:01 -0500

    Created buttons, added action listeners, and prepared action listener section

--- Java-Calculator/CalculatorGUI.java
+++ Java-Calculator/CalculatorGUI.java
@@ -51,13 +51,60 @@ public class CalculatorGUI extends JFrame implements ActionListener {
         setLayout(new GridBagLayout());
         GridBagConstraints position = new GridBagConstraints();
         
-        // creates text display area
+        // creates taskbar items 
+        menu = new JMenuBar();
+        fileMenu = new JMenu("File");
+        quitItem = new JMenuItem("Quit");
         
+        fileMenu.add(quitItem);
+        menu.add(fileMenu);
+        setJMenuBar(menu);
+        
+        // creates text display area
         display = new TextArea();
+        add(display);
         
         // creates buttons
+        zero = new JButton("0");
+        one = new JButton("1");
+        two = new JButton("2");
+        three = new JButton("3");
+        four = new JButton("4");
+        five = new JButton("5");
+        six = new JButton("6");
+        seven = new JButton("7");
+        eight = new JButton("8");
+        nine = new JButton("9");
+        add = new JButton("+");
+        sub = new JButton("-");
+        mul = new JButton("x");
+        div = new JButton("/");
+        clear = new JButton("c");
+        sqrt = new JButton("sqrt");
+        percent = new JButton("%");
+        period = new JButton(".");
         
+        // adds action listener to buttons and menu items
+        quitItem.addActionListener(this);
         
+        zero.addActionListener(this);
+        one.addActionListener(this);
+        two.addActionListener(this);
+        three.addActionListener(this);
+        four.addActionListener(this);
+        five.addActionListener(this);
+        six.addActionListener(this);
+        seven.addActionListener(this);
+        eight.addActionListener(this);
+        nine.addActionListener(this);
+        add.addActionListener(this);
+        sub.addActionListener(this);
+        mul.addActionListener(this);
+        div.addActionListener(this);
+        clear.addActionListener(this);
+        sqrt.addActionListener(this);
+        percent.addActionListener(this);
+        period.addActionListener(this);
     }
     
     /**
@@ -68,6 +115,60 @@ public class CalculatorGUI extends JFrame implements ActionListener {
     public void actionPerformed (ActionEvent e) {
         if (e.getSource() == quitItem) {
             System.exit(1);
+        }
+        if (e.getSource() == zero) {
+            
+        }
+        if (e.getSource() == one) {
+            
+        }
+        if (e.getSource() == two) {
+            
+        }
+        if (e.getSource() == three) {
+            
+        }
+        if (e.getSource() == four) {
+            
+        }
+        if (e.getSource() == five) {
+            
+        }
+        if (e.getSource() == six) {
+            
+        }
+        if (e.getSource() == seven) {
+            
+        }
+        if (e.getSource() == eight) {
+            
+        }
+        if (e.getSource() == nine) {
+            
+        }
+        if (e.getSource() == add) {
+            
+        }
+        if (e.getSource() == sub) {
+            
+        }
+        if (e.getSource() == mul) {
+            
+        }
+        if (e.getSource() == div) {
+            
+        }
+        if (e.getSource() == clear) {
+            
+        }
+        if (e.getSource() == sqrt) {
+            
+        }
+        if (e.getSource() == percent) {
+            
+        }
+        if (e.getSource() == period) {
+            
         }
     }
     
